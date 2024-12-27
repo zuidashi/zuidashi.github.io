100ef486f2d856a3e100e2040b888d20c407e3cb
zuidashi <zuiqinghe@hotmail.com>
2024-12-27 04:40:11 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [1](https://zuidashi.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 19 
-### :alarm_clock: 2024-12-27 12:19:41 
+### :alarm_clock: 2024-12-27 12:40:11 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/plugins/RonanTheme.js
+++ docs/plugins/RonanTheme.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         html {    
-            background: url('https://blog.freeblock.cn/background.webp') no-repeat center center fixed;
+            background: url('https://zuidashi.github.io/background.webp') no-repeat center center fixed;
             background-size: cover;
         }
 
