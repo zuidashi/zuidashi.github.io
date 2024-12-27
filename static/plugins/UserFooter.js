70f3e575be62b6ba4924d89dd4b4320c75e79d70
zuidashi <90899297+zuidashi@users.noreply.github.com>
2024-12-27 18:06:37 +0800

    Update UserFooter.js

--- static/plugins/UserFooter.js
+++ static/plugins/UserFooter.js
@@ -1,6 +1,6 @@
 function userfooter() {
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="userfooter" style="display:none">blog.aklin.cn</span>');
+    runday.insertAdjacentHTML('afterend', '<span id="userfooter">blog.aklin.cn</span>');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
