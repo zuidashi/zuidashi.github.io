ca1a1f99ecc1648f15b784ca82615e8fd34a71ef
zuidashi <90899297+zuidashi@users.noreply.github.com>
2024-12-27 13:47:12 +0800

    Update GmeekVercount.js

--- static/plugins/GmeekVercount.js
+++ static/plugins/GmeekVercount.js
@@ -4,7 +4,7 @@ function createVercount() {
         postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">当前文章已查看<span id="busuanzi_value_page_pv"></span>次</div>');
     }
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">已被浏览<span id="busuanzi_value_site_pv"></span>次</span>');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">已被浏览<span id="busuanzi_value_site_pv"></span>次  </span>');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
