444e512748c9468a2f33f279f88698de2fc54ba3
zuidashi <90899297+zuidashi@users.noreply.github.com>
2024-12-28 11:16:07 +0800

    Delete static/plugins/UserFooter.js

--- static/plugins/UserFooter.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="userfooter"> 友情连接 • 免责声明 • 关于本站 • </span>');
-});
