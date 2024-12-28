e46036f3c59054f49b2e539e4a8a1429514535c8
zuidashi <90899297+zuidashi@users.noreply.github.com>
2024-12-28 11:14:27 +0800

    Update UserFooter.js

--- static/plugins/UserFooter.js
+++ static/plugins/UserFooter.js
@@ -1,8 +1,4 @@
-function userfooter() {
+document.addEventListener("DOMContentLoaded", function() {
     var runday = document.getElementById('runday');
     runday.insertAdjacentHTML('afterend', '<span id="userfooter"> 友情连接 • 免责声明 • 关于本站 • </span>');
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    userfooter();
 });
